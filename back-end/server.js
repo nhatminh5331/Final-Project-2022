e7c9b501bf94a19740c85eecf89bfc67e0acb567
= <=>
2021-08-15 16:59:14 +0700

    test commit

--- back-end/server.js
+++ back-end/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.json({ msg: "Hello" });
+  res.json({ msg: "Hello World" });
 });
 
 const URL = process.env.MONGODB_URL;
