cc84bc6cbaba7c1dbb5efbfb3c1ecd484d052fc9
= <=>
2021-08-15 16:26:55 +0700

    set up middleware connect db and package reactjs

---
+++ front-end/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <input type="checkbox" id="theme" />
+      <div className="App">
+        <div className="main">
+          <h1>Hello World!</h1>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
