f8a93536d8d5ebd3334fa58039cb891cf3b2b4ca
= <=>
2021-08-16 20:26:11 +0700

    update model for user

---
+++ back-end/models/userModel.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 30,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 30,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://i.pinimg.com/originals/e2/7c/87/e27c8735da98ec6ccdcf12e258b26475.png",
+    },
+    mobile: {
+      type: String,
+      default: "",
+    },
+    address: {
+      type: String,
+      default: "",
+    },
+    story: {
+      type: String,
+      default: "",
+      maxlength: 140,
+    },
+    followers: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("user", userSchema);
