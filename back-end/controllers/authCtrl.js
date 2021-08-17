ec8174f754fa671d23179f8e1e33af8d8aa89257
= <=>
2021-08-18 00:05:39 +0700

    Update register controller and set up accesstoken, refreshtoken

--- back-end/controllers/authCtrl.js
+++ back-end/controllers/authCtrl.js
@@ -6,8 +6,42 @@ const authCtrl = {
   register: async (req, res) => {
     try {
       const { fullname, username, email, password } = req.body;
-      console.log(req.body);
-      res.json({ msg: "Registed !" });
+      let newUserName = username.toLowerCase().replace(/ /g, "");
+
+      const user_name = await Users.findOne({ username: newUserName });
+      if (user_name)
+        return res.status(400).json({ msg: "This username already exists." });
+
+      const user_email = await Users.findOne({ email });
+      if (user_email)
+        return res.status(400).json({ msg: "This email already exists." });
+
+      if (password.length < 6)
+        return res
+          .status(400)
+          .json({ msg: "Password must be at least 6 characters" });
+
+      const passwordHash = await bcrypt.hash(password, 12);
+
+      const newUser = new Users({
+        fullname,
+        username: newUserName,
+        email,
+        password: passwordHash,
+      });
+
+      const access_token = createAccessToken({ id: newUser._id });
+      const refresh_token = createRefreshToken({ id: newUser._id });
+
+      res.json({
+        msg: "Register thành công rồi !",
+        access_token,
+        user: {
+          //_doc để trả lại thông tin cần thiết người dùng
+          ...newUser._doc,
+          //password: "",
+        },
+      });
     } catch (error) {
       return res.status(500).json({ msg: error.message });
     }
@@ -30,6 +64,18 @@ const authCtrl = {
       return res.status(500).json({ msg: error.message });
     }
   },
+};
+//Access Token
+const createAccessToken = (payload) => {
+  return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1d",
+  });
+};
+//Refresh Token
+const createRefreshToken = (payload) => {
+  return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "42d",
+  });
 };
 
 module.exports = authCtrl;
