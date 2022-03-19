811ba12c82f41d030d61e8394907f125c9e2f29a
= <=>
2022-03-19 17:25:04 +0700

    Update and done view list all user

--- front-end/src/pages/allUser/allUserItem.js
+++ front-end/src/pages/allUser/allUserItem.js
@@ -1,10 +1,44 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import './allUser.css'
 
 const AllUserItem = ({users}) => {
   return (
       <div>
-        Username: {users.username}
-        Email: {users.email}
+          <table className="table_user">
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Avatar</th>
+                  <th>Username</th>
+                  <th>Email</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                  <td>{users._id}</td>
+                  <td>
+                  <Link to={`/profile/${users._id}`} className="text-dark">
+                    <img src={users.avatar} alt={users.avatar} className="small-avatar"/>
+                  </Link>
+                  </td>
+                  <td>
+                  <Link to={`/profile/${users._id}`} className="text-dark">
+                    {users.username}
+                  </Link>
+                  </td>
+                  <td>{users.email}</td>
+                  <td>
+                    {
+                      users.role === 0
+                      ? <i className="fas fa-trash-alt" title="Remove"></i>
+                      : <>
+                          <p>Admin</p>
+                        </>
+                    }
+                  </td>
+              </tbody>
+          </table>
       </div>
   )
 }
